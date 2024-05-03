df427e735ca063b0e218c37354dcf2c28a2aeec9
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-05-03 14:38:27 +0100

    correction on task-101

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,7 +4,7 @@ import typing
 
 
 def safe_first_element(lst: typing.Sequence[typing.Any]) -> \
-    typing.Union[typing.Any, None]:
+        typing.Union[typing.Any, None]:
     """100-safe_first_element.py"""
     if lst:
         return lst[0]
