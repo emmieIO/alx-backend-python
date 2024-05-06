d90648dc5f44111a97d8103aa19a8cf5606563ef
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-05-06 13:44:13 +0100

    pushing task-0&1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """1-concurrent_coroutines.py"""
 import asyncio
+import typing
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: float) -> List[float]:
     """This asynchronous coroutine spawns n wait_random
     coroutines concurrently
     with the specified max_delay and returns a
