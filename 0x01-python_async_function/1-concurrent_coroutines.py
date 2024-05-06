794c8351f1d1182076688547cc745ee0a907ad0b
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-05-06 13:47:03 +0100

    pushing task-0&1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
-"""1-concurrent_coroutines.py"""
+"""
+1-concurrent_coroutines.py
+"""
 import asyncio
-import typing
-
-
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: float) -> List[float]:
-    """This asynchronous coroutine spawns n wait_random
-    coroutines concurrently
-    with the specified max_delay and returns a
-    list of delays in ascending order.
-    """
-    delays = []
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn wait_random n times"""
     tasks = []
-    for _ in range(n):
+    delays = []
+
+    for i in range(n):
         task = wait_random(max_delay)
         tasks.append(task)
-        
-    for task in asyncio.as_completed(tasks):
+
+    for task in asyncio.as_completed((tasks)):
         delay = await task
         delays.append(delay)
+
     return delays
